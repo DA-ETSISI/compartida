0671f8c72ade354a8fc4a646588aa8f6d0527b83
pdrj09 <pedro.yt.99@gmail.com>
2025-04-15 15:46:08 +0200

    feat: ejercicios model

--- apuntes/models.py
+++ apuntes/models.py
@@ -1,30 +1,71 @@
 """
-Modelos de la db de la app apuntes.
+This module defines two Django models: Apunte and ejercicios.
+
+Models:
+    - Apunte: Represents a study note or document uploaded by a user. It includes attributes 
+      such as title, file path, creation date, associated subject, download count, uploader, 
+      associated professors, and a description.
+
+    - ejercicios: Represents an exercise with attributes such as statement, optional PDF file, 
+      creation date, associated subject, associated topic, view count, and uploader.
 """
 from django.db import models
-from usrs.models import Asignatura, UsrDa, Profesor
+from usrs.models import Asignatura, UsrDa, Profesor, Tema
 
 class Apunte(models.Model):
     """
-    Modelo para almacenar apuntes.
+    Apunte Model
 
-    atributos:
-    - titulo (str): Título del apunte.
-    - pdfdir (dir => str): dirección de el pdf de los apuntes (apuntes/).
-    - fecha_creacion (dateTime): Fecha de creación del apunte.
-    - asignatura (asignatura_id => int): Asignatura a la que pertenece el apunte => enlace a la 
-        tabla asignatura, Si se borra el modelo de referencia se pone a null.
-    - descargas (int): Número de descargas del apunte.
-    - user (user_id => int): Usuario que subió el apunte => enlace a la tabla user, Si se borra el
-        usuario se borran los apuntes.
-    - apoyo_docente (user_id => int): Usuario que subió el apunte => enlace a la tabla user, Si se
-        borra el usuario se borran los apuntes.
+    Represents a study note or document uploaded by a user.
+
+    Attributes:
+        titulo (CharField): The title of the note, limited to 255 characters.
+        pdfdir (FileField): The file path to the uploaded PDF document.
+        fecha_creacion (datetime): The timestamp when the note was created. 
+            Automatically set on creation.
+        asignatura (ForeignKey): A foreign key to the Asignatura model, representing the 
+            subject associated with the note. Can be null if not assigned to a subject.
+        descargas (IntegerField): The number of times the note has been downloaded. Defaults to 0.
+        user (ForeignKey): A foreign key to the UsrDa model, representing the user who 
+            uploaded the note.
+        apoyo_docente (ManyToManyField): A many-to-many relationship with the Profesor model,
+            representing the professors who support or are associated with the note.
+        descipcion (CharField): A textual description of the note. Defaults to an empty string.
     """
+
     titulo = models.CharField(max_length=255)
-    pdfdir = models.FileField(upload_to='uploads/')
+    pdfdir = models.FileField(upload_to='uploads/apuntes')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     asignatura = models.ForeignKey(Asignatura, on_delete=models.SET_NULL, null=True)
     descargas = models.IntegerField(default=0)
     user = models.ForeignKey(UsrDa, on_delete=models.CASCADE)
     apoyo_docente = models.ManyToManyField(Profesor)
     descipcion = models.TextField(default="")
+
+class Ejercicios(models.Model):
+    """
+    Model representing an exercise (ejercicio).
+
+    Attributes:
+        enunciado (TextField): The statement or description of the exercise.
+        pdfdir (FileField): Optional file field to upload a PDF related to the exercise.
+            Files are uploaded to the 'uploads/ejercicios' directory.
+        fecha_creacion (DateTimeField): The timestamp when the exercise was created.
+            Automatically set to the current date and time when created.
+        asignatura (ForeignKey): A foreign key linking the exercise to an Asignatura instance.
+            Allows null values and sets the field to NULL if the related Asignatura is deleted.
+        tema (ForeignKey): A foreign key linking the exercise to a Tema instance.
+            Allows null values and sets the field to NULL if the related Tema is deleted.
+        visualizaciones (IntegerField): The number of times the exercise has been viewed.
+            Defaults to 0.
+        user (ForeignKey): A foreign key linking the exercise to a UsrDa instance.
+            Deleting the related UsrDa will also delete the exercise.
+    """
+
+    enunciado = models.TextField()
+    pdfdir = models.FileField(upload_to='uploads/ejercicios', blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.SET_NULL, null=True)
+    tema =  models.ForeignKey(Tema, on_delete=models.SET_NULL, null=True)
+    visualizaciones = models.IntegerField(default=0)
+    user = models.ForeignKey(UsrDa, on_delete=models.CASCADE)
--- usrs/models.py
+++ usrs/models.py
@@ -1,30 +1,65 @@
 """
-Modelos de la app usrs.
+This module defines the models for a Django application that manages academic subjects, 
+degrees, users, and professors. It includes the following models:
+
+Models:
+    - Asignatura: Represents an academic subject with attributes for name, credits, and semester.
+    - Tema: Represents a topic associated with a specific subject.
+    - Titulacion: Represents an academic degree or program with associated subjects and credits.
+    - UsrDa: Represents a user in the system with extended attributes and permissions.
+    - Profesor: Represents a professor with assigned subjects.
+
+Models Managers:
+    - UsrDaManager: Custom manager for creating users and superusers with additional logic.
+
+Each model includes detailed attributes and relationships to represent the academic structure 
+and user roles within the system.
 """
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
 class Asignatura(models.Model):
     """
-    Modelo para almacenar asignaturas.
+    Model representing an academic subject.
 
-    atributos:
-    - nombre: Nombre de la asignatura.
-    - créditos: Número de créditos de la asignatura.
-
+    Attributes:
+        nombre (CharField): The name of the subject, with a maximum length of 255 characters.
+        creditos (IntegerField): The number of credits assigned to the subject.
+        semestre (IntegerField): The semester in which the subject is offered.
     """
+
     nombre = models.CharField(max_length=255)
-    creditos = models.IntegerField()
+    creditos = models.IntegerField(default=6)
+    semestre = models.IntegerField(default=0)
+
+class Tema(models.Model):
+    """
+    Represents a topic (Tema) associated with a specific subject (Asignatura).
+
+    Attributes:
+        nombre (CharField): The name of the topic, limited to 255 characters.
+        asignatura (ForeignKey): A foreign key linking the topic to an instance of the 
+            Asignatura model. Deleting the associated Asignatura will cascade and 
+            delete the related Tema instances.
+    """
+
+    nombre = models.CharField(max_length=255)
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
 
 class Titulacion(models.Model):
     """
-    Modelo para almacenar titulación.
+    Represents an academic degree or program.
 
-    atributos:
-    - nombre: Nombre de la carrera.
-
+    Attributes:
+        nombre (CharFiled): The name of the degree or program, with a maximum length of 255 characters.
+        asignaturas (ManyToManyField): A many-to-many relationship with the Asignatura model, 
+            representing the subjects associated with the degree. This field is optional.
+        creditos (IntegerField): The total number of credits required for the degree.
     """
+
     nombre = models.CharField(max_length=255)
+    asignaturas = models.ManyToManyField(Asignatura, blank=True)
+    creditos = models.IntegerField(default=270)
 
 class UsrDaManager(BaseUserManager):
     """
@@ -32,8 +67,25 @@ class UsrDaManager(BaseUserManager):
     """
     def create_user(self, preferred_username, email, es_profesor=False, **extra_fields):
         """
-        Metodo para crear un usuario normal.
+        Creates and returns a new user with the specified username, email, and additional fields.
+
+        Args:
+            preferred_username (str): SIU username for the new user.
+            email (str): The email address for the new user.
+            es_profesor (bool, optional): Indicates whether the user is a professor. 
+                Defaults to False.
+            **extra_fields: Additional fields to include when creating the user.
+
+        Returns:
+            user: The created user instance.
+
+        Side Effects:
+            - If `es_profesor` is True, a `Profesor` instance is created and associated 
+                with the user.
+            - The user's password is set to an unusable state.
+            - The user instance is saved to the database.
         """
+
         email = self.normalize_email(email)
         user = self.model(
             preferred_username=preferred_username,
@@ -49,30 +101,64 @@ class UsrDaManager(BaseUserManager):
 
     def create_superuser(self, preferred_username, email, **extra_fields):
         """
-        Metodo para crear un superusuario.
+        Creates and returns a superuser with the given preferred username, email, 
+        and additional fields.
+
+        This method ensures that the created user has the 'is_staff' and 
+        'is_superuser' flags set to True.
+
+        Args:
+            preferred_username (str): The preferred username for the superuser.
+            email (str): The email address for the superuser.
+            **extra_fields: Additional fields to set on the superuser.
+
+        Returns:
+            User: The created superuser instance.
         """
+
+
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(preferred_username, email, **extra_fields)        
+        return self.create_user(preferred_username, email, **extra_fields)
 
 class UsrDa(AbstractBaseUser, PermissionsMixin):
     """
-    Modelo de usuario personalizado que hereda de AbstractUser => django.user + usrs.user.
-    Se pueden añadir campos adicionales si es necesario.
-    atributos:
-    - Titulación (titulacion_id): Relación con el modelo titulación.
-    - Recuento de subidas (int): Número de apuntes subidos por el usuario.
-    - Recuento de descargas (int): Número de apuntes descargados por el usuario.
-    - Es profesor (Bool): Booleano que indica si el usuario es profesor o no.
+    UsrDa Model
+
+    This model represents a user in the system with extended attributes and permissions.
+
+    Attributes:
+        preferred_username (CharField): The preferred username of the user. Must be unique.
+        email (CharField): The email address of the user. Must be unique.
+        name (CharField): The full name of the user.
+        given_name (CharField): The given name (first name) of the user.
+        family_name (CharField): The family name (last name) of the user.
+        UPMClassCode (CharField): A code provided by Keycloak (siu) for the user.
+        titulacion (ForeignKey): A foreign key to the `Titulacion` model, 
+            representing the user's degree program.
+        recuento_subidas (int): The count of uploads made by the user. Defaults to 0.
+        recuento_descargas (int): The count of downloads made by the user. Defaults to 0.
+        es_profesor (bool): A flag indicating whether the user is a professor. Defaults to False.
+        password (CharField): The password for the user. Can be blank or null.
+
+    Meta:
+        USERNAME_FIELD: Specifies the field used as the unique identifier for authentication, 
+            set to `preferred_username`.
+
+    Manager:
+        objects: Custom manager for the `UsrDa` model, provided by `UsrDaManager`.
     """
 
     preferred_username = models.CharField(max_length=255, unique=True, default="")
     email = models.EmailField(unique=True, default="")
+    name = models.CharField(max_length=255, default="")
     given_name = models.CharField(max_length=255, default="")
     family_name = models.CharField(max_length=255, default="")
+
+    # Código upm dado por keycloak (siu)
     UPMClassCode = models.CharField(max_length=255, default="")
-    name = models.CharField(max_length=255, default="")
+
     titulacion = models.ForeignKey(Titulacion, on_delete=models.SET_NULL, null=True)
     recuento_subidas = models.IntegerField(default=0)
     recuento_descargas = models.IntegerField(default=0)
@@ -86,12 +172,13 @@ class UsrDa(AbstractBaseUser, PermissionsMixin):
 
 class Profesor(models.Model):
     """
-    Modelo para almacenar profesores.
-    atributos:
-    - user (user_id => int): Usuario que subió el apunte => enlace a la tabla user, Si se borra el
-        usuario se borran los apuntes.
-    - asignaturas (asignatura_id => int): Asignaturas impartidas por el profesor => enlace a la
-        tabla asignatura, Si se borra la asignatura se pone a null.
+    Represents a Profesor model that associates a user with their assigned subjects.
+
+    Attributes:
+        user (OneToOneField): A one-to-one relationship linking the Profesor to a UsrDa instance.
+        asignaturas (ManyToManyField): A many-to-many relationship linking the Profesor 
+            to multiple Asignatura instances.
     """
+
     user = models.OneToOneField(UsrDa, on_delete=models.CASCADE)
     asignaturas = models.ManyToManyField(Asignatura)
