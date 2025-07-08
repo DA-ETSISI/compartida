b774282109f2c88d93add7a8c842dd446ac5f5f7
pdrj09 <pedro.yt.99@gmail.com>
2025-07-09 00:23:37 +0200

    fix: usrs models

--- backend/usrs/models.py
+++ backend/usrs/models.py
@@ -17,6 +17,42 @@ and user roles within the system.
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
+
+class Titulacion(models.Model):
+    """
+    Represents an academic degree or program.
+
+    Attributes:
+        nombre (CharFiled): The name of the degree.
+        codigo (CharField): A unique code for the degree, limited to 10 characters.
+    """
+
+    nombre = models.CharField(max_length=255)
+    codigo = models.CharField(max_length=10, default="", unique=True)
+    tipo = models.CharField(max_length=10, default="GRA", blank=True)
+
+class Asignatura(models.Model):
+    """
+    Model representing an academic subject.
+
+    Attributes:
+        nombre (CharField): The name of the subject, with a maximum length of 255 characters.
+        creditos (IntegerField): The number of credits assigned to the subject.
+        curso (IntegerField): The course number in which the subject is taught.
+        optativa (BooleanField): Indicates whether the subject is optional (True) or mandatory (
+            False). Defaults to False.
+        grados (ManyToManyField): A many-to-many relationship with the Titulacion model, 
+            representing the degrees associated with the subject. This field is required.
+    """
+    codigo = models.CharField(max_length=20, primary_key=True, unique=True, default="")
+
+    nombre = models.CharField(max_length=255)
+    creditos = models.IntegerField(default=6)
+    curso = models.IntegerField(default=0)
+    optativa = models.BooleanField(default=False)
+
+    grados =  models.ManyToManyField('Titulacion', symmetrical=False, blank=False)
+
 class UsrDaManager(BaseUserManager):
     """
     Clase para gestionar la creación de usuarios y superusuarios.
@@ -126,7 +162,7 @@ class UsrDa(AbstractBaseUser, PermissionsMixin):
     codigos_escuela = models.JSONField(default=list)
     tipo_usuario = models.JSONField(default=list)
 
-    titulacion = models.ForeignKey('Titulacion', on_delete=models.SET_NULL, blank= True, null=True)
+    titulacion = models.ForeignKey(Titulacion, on_delete=models.SET_NULL, blank= True, null=True)
     recuento_subidas = models.IntegerField(default=0)
     recuento_visualizaciones = models.IntegerField(default=0)
     es_profesor = models.BooleanField(default=False)
@@ -158,4 +194,4 @@ class Profesor(models.Model):
     """
 
     user = models.OneToOneField(UsrDa, on_delete=models.CASCADE)
-    asignaturas = models.ManyToManyField('Asignatura', blank=True)
+    asignaturas = models.ManyToManyField(Asignatura, blank=True)
