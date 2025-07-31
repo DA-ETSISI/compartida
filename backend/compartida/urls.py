104b0ece2030a99054e81e896253a18125da19a5
pdrj09 <pedro.yt.99@gmail.com>
2025-07-31 23:11:55 +0200

    feat: protect swagger docs with staff-only access

--- backend/compartida/urls.py
+++ backend/compartida/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.permissions import IsAdminUser
 
 urlpatterns = [
     path("apuntes/", include("apuntes.urls")),
@@ -8,6 +9,16 @@ urlpatterns = [
     # Siu login
     path("usrs/login/", include("mozilla_django_oidc.urls")),
     # Swagger, auth, etc.
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "schema/",
+        SpectacularAPIView.as_view(permission_classes=[IsAdminUser]),
+        name="schema",
+    ),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(
+            url_name="schema", permission_classes=[IsAdminUser]
+        ),
+        name="swagger-ui",
+    ),
 ]
